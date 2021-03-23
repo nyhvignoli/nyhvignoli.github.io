fff016fa9b8270d153297e7d44b32f2dec156097
nyhvignoli <nyhv.contato@gmail.com>
2021-03-23 17:27:01 -0300

    knowledges-section

--- src/global/styles.js
+++ src/global/styles.js
@@ -2,7 +2,16 @@ import styled from "styled-components";
 
 export const Title = styled.h1`
     font-size: 6rem;
-    line-height: 2rem;
+    font-family: 'Roboto Condensed', sans-serif;
+    height: 0.8em;
+    /* font-family: 'Syne Mono', monospace; */
+    /* font-family: 'Ubuntu Condensed', sans-serif;  */
+    /* font-family: 'Pathway Gothic One', sans-serif; */
+`;
+
+export const Subtitle = styled.h2`
+    font-size: 3rem;
+    opacity: 0.8;
     font-family: 'Roboto Condensed', sans-serif;
     /* font-family: 'Syne Mono', monospace; */
     /* font-family: 'Ubuntu Condensed', sans-serif;  */
