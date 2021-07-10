ba9f5740ff4f9c1c26f75b8731d22f837262b97c
nyhvignoli <nyhv.contato@gmail.com>
2021-07-10 16:47:36 -0300

    knowledges section translations

--- src/components/Knowledges/Knowledges.js
+++ src/components/Knowledges/Knowledges.js
@@ -1,60 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Subtitle } from "../../global/styles";
 import { ListItem, StackList } from "../../global/styles";
 import KnowledgesContent from "./KnowledgesContent/KnowledgesContent";
 import { Wrapper, Content } from "./styles";
 import backend from "../../assets/backend.png";
 import frontend from "../../assets/frontend.svg";
+import { LanguageContext, Text } from "../../global/LanguagesContext";
 
 const Knowledges = () => {
-    const frontEndKnowledges = [
-        "Desenvolvimento de aplicações Web em HTML, CSS e Javascript",
-        "Componentização e manipulação de estado com ReactJS",
-        "Integrações com API's REST",
-        "Layouts responsivos e bibliotecas de estilização como Styled-components e Material-UI"
-    ];
-
-    const backEndKnowledges = [
-        "Desenvolvimento de API's RESTful utilizando Node.js e Typescript",
-        "Testes unitários e automatizados com Jest",
-        "Banco de dados MySQL",
-        "Familiaridade com Firebase"
-    ];
-
-    const generalKnowledges = [
-        "AWS e EC2",
-        "Metodologias Ágeis",
-        "Clean Code",
-    ];
+    const { dictionary } = useContext(LanguageContext)
 
     return (
         <Wrapper>
-            <Subtitle data-aos="fade-zoom-in" data-aos-duration="1500" data-aos-easing="ease-in-out">Conhecimentos</Subtitle>
+            <Subtitle data-aos="fade-zoom-in" data-aos-duration="1500" data-aos-easing="ease-in-out"><Text tid="knowledges"/></Subtitle>
             <Content>
                 <KnowledgesContent
                     title={"Front-end"} 
-                    knowledges={frontEndKnowledges}
+                    knowledges={dictionary.frontendKnowledges}
                     icon={frontend}
                     alt="Ícone de diferentes telas e dispositivos"
                 />
                 <KnowledgesContent 
                     title={"Back-end"}
-                    knowledges={backEndKnowledges}
+                    knowledges={dictionary.backendKnowledges}
                     icon={backend}
                     alt="Ícone de uma tela com engrenagens"
                 />
             </Content>
             <div>
-                <Subtitle data-aos="fade-zoom-in" data-aos-duration="1500">Gerais</Subtitle>
+                <Subtitle data-aos="fade-zoom-in" data-aos-duration="1500"><Text tid="general"/></Subtitle>
                 <StackList>
-                    {generalKnowledges.map((item, index) => {
+                    {dictionary.generalKnowledges.map((item, index) => {
                         return (
                             <ListItem data-aos="flip-right" data-aos-duration="2000" data-aos-easing="ease-out-cubic" key={index}>{item}</ListItem>
                         )
                     })}
                 </StackList>
             </div>
-            
         </Wrapper>
     );
 }
