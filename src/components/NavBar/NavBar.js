bc838c365e2217fb957f46c49f1a1e8718db7567
nyhvignoli <nyhv.contato@gmail.com>
2021-07-11 13:44:28 -0300

    testing styling languages selector

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text } from "../../global/LanguagesContext";
+import { BaseFlex } from "../../global/styles";
 import LanguageSelector from "../LanguageSelector/LanguageSelector";
 import { StyledNav, StyledList, ListItem, Anchor } from "./styles";
 
@@ -7,12 +8,14 @@ const NavBar = () => {
 
     return (
         <StyledNav>
-            <StyledList>
+            <BaseFlex>
                 <LanguageSelector />
-                <Anchor href="#top"><ListItem><Text tid="aboutMe"/></ListItem></Anchor>
-                <Anchor href="#projects"><ListItem><Text tid="projects"/></ListItem></Anchor>
-                <Anchor href="#contact"><ListItem><Text tid="contactMe"/></ListItem></Anchor>
-            </StyledList>
+                <StyledList>
+                    <Anchor href="#top"><ListItem><Text tid="aboutMe"/></ListItem></Anchor>
+                    <Anchor href="#projects"><ListItem><Text tid="projects"/></ListItem></Anchor>
+                    <Anchor href="#contact"><ListItem><Text tid="contactMe"/></ListItem></Anchor>
+                </StyledList>
+            </BaseFlex>
         </StyledNav>
     );
 }
