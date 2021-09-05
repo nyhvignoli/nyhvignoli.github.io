969196ff62238ea3f74b0c946788f1ec4671c4a4
nyhvignoli <nyhv.contato@gmail.com>
2021-09-05 17:55:53 -0300

    add mobile drawer menu

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react'
 import { Text } from '../../global/LanguagesContext'
-import { BaseFlex } from '../../global/styles'
 import LanguageSelector from '../LanguageSelector/LanguageSelector'
-import { StyledNav, LanguageMenu, StyledList, ListItem, Anchor } from './styles'
+import {
+  StyledNav,
+  LanguageMenu,
+  MenuIcon,
+  StyledList,
+  ListItem,
+  Anchor
+} from './styles'
 import languagesIcon from '../../assets/languages.png'
+import menuIcon from '../../assets/menu.png'
+import closeIcon from '../../assets/cancel.png'
 
 const NavBar = () => {
   const [active, setActive] = useState(undefined)
   const [open, setOpen] = useState(false)
+  const [drawerOpen, setDrawerOpen] = useState(false)
 
   const handleActive = (menuItem) => {
     setActive(menuItem)
+    setDrawerOpen(!drawerOpen)
   }
 
   const showSelect = () => {
@@ -21,46 +31,53 @@ const NavBar = () => {
     setOpen(false)
   }
 
+  const showDrawer = () => {
+    setDrawerOpen(!drawerOpen)
+  }
+
   return (
     <StyledNav>
-      <BaseFlex>
-        <LanguageMenu onMouseOver={showSelect} onMouseLeave={hideSelect}>
-          <img src={languagesIcon} alt="World map icon" />
-          <LanguageSelector show={open} hideSelect={hideSelect} />
-        </LanguageMenu>
-        <StyledList>
-          <Anchor
-            data-testid="about-me-item"
-            onClick={() => handleActive('about-me')}
-            active={active === 'about-me'}
-            href="#top"
-          >
-            <ListItem>
-              <Text tid="aboutMe" />
-            </ListItem>
-          </Anchor>
-          <Anchor
-            data-testid="projects-item"
-            onClick={() => handleActive('projects')}
-            active={active === 'projects'}
-            href="#projects"
-          >
-            <ListItem>
-              <Text tid="projects" />
-            </ListItem>
-          </Anchor>
-          <Anchor
-            data-testid="contact-me-item"
-            onClick={() => handleActive('contact-me')}
-            active={active === 'contact-me'}
-            href="#contact"
-          >
-            <ListItem>
-              <Text tid="contactMe" />
-            </ListItem>
-          </Anchor>
-        </StyledList>
-      </BaseFlex>
+      <LanguageMenu onMouseOver={showSelect} onMouseLeave={hideSelect}>
+        <img src={languagesIcon} alt="World map icon" />
+        <LanguageSelector show={open} hideSelect={hideSelect} />
+      </LanguageMenu>
+      <StyledList hide={!drawerOpen}>
+        <Anchor
+          data-testid="about-me-item"
+          onClick={() => handleActive('about-me')}
+          active={active === 'about-me'}
+          href="#top"
+        >
+          <ListItem>
+            <Text tid="aboutMe" />
+          </ListItem>
+        </Anchor>
+        <Anchor
+          data-testid="projects-item"
+          onClick={() => handleActive('projects')}
+          active={active === 'projects'}
+          href="#projects"
+        >
+          <ListItem>
+            <Text tid="projects" />
+          </ListItem>
+        </Anchor>
+        <Anchor
+          data-testid="contact-me-item"
+          onClick={() => handleActive('contact-me')}
+          active={active === 'contact-me'}
+          href="#contact"
+        >
+          <ListItem>
+            <Text tid="contactMe" />
+          </ListItem>
+        </Anchor>
+      </StyledList>
+      <MenuIcon
+        onClick={showDrawer}
+        src={drawerOpen ? closeIcon : menuIcon}
+        alt="menu icon"
+      />
     </StyledNav>
   )
 }
--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -6,19 +6,36 @@ const { colors } = theme
 export const StyledNav = styled.nav`
   position: sticky;
   top: 0;
-  z-index: 1000;
+  z-index: 3000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 0.5rem 2rem;
   background-color: ${colors.transparentBlue};
   box-shadow: 2px 4px 6px 2px rgba(0, 0, 0, 0.2);
   color: ${colors.white};
   font-family: 'Roboto Condensed', sans-serif;
   font-weight: bold;
+
+  img {
+    width: 38px;
+  }
+
+  @media (max-width: 480px) {
+    padding: 0.5rem;
+  }
 `
 
 export const LanguageMenu = styled.div`
+  justify-self: flex-start;
   position: relative;
-  img {
-    width: 38px;
+`
+
+export const MenuIcon = styled.img`
+  transition: all 0.4s ease-in-out 0.1s;
+
+  @media (min-width: 480px) {
+    display: none;
   }
 `
 
@@ -29,10 +46,29 @@ export const StyledList = styled.ul`
   padding-inline-start: 0;
   gap: 3rem;
 
-  @media (max-width: 800px) {
-    font-size: 1rem;
+  @media (max-width: 480px) {
+    font-size: 2rem;
     justify-content: center;
-  }
+    align-items: center;
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    left: 30%;
+    flex-direction: column;
+    background-color: black;
+    height: 100vh;
+    width: 80vw;
+    margin: 0;
+    z-index: -1;
+    transition: all 0.2s ease-in-out 0.1s;
+    text-align: center;
+
+    ${({ hide }) =>
+      hide &&
+      `
+        transition: all 0.4s ease-in-out 0.1s;
+        left: 100%;
+      `}
 `
 
 export const ListItem = styled.li``
