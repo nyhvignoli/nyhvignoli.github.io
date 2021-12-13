82b7908579303ca22d440c7f07f6bff17b5493c8
nyhvignoli <nyhv.contato@gmail.com>
2021-12-13 16:50:46 -0300

    moved white-arrow to system-icons folder

--- src/assets/system-icons/index.js
+++ src/assets/system-icons/index.js
@@ -1,5 +1,6 @@
 import cancelIcon from './cancel.png'
 import languagesIcon from './languages.png'
 import menuIcon from './menu.png'
+import whiteArrow from './white-arrow.png'
 
-export { cancelIcon, languagesIcon, menuIcon }
+export { cancelIcon, languagesIcon, menuIcon, whiteArrow }
