128b3c1c0a59ad03436f803b146b9f1f7fa86962
nyhvignoli <nyhv.contato@gmail.com>
2023-03-13 12:33:17 -0300

    tokens(theme): Add tokens for breakpoints, color, spacing, border, shadow, transition and fonts

---
+++ src/theme/breakpoints.js
@@ -0,0 +1,16 @@
+export const Breakpoints = {
+  MOBILE: {
+    X_SMALL: 'max-width: 319px', // smaller than 319px
+    SMALL: 'min-width: 320px', // between 320px and 359px
+    REGULAR: 'min-width: 360px', // between 360px and 410px
+    LARGE: 'min-width: 411px' // between 411px and 480px
+  },
+  TABLET: {
+    REGULAR: 'min-width: 481px' // between 481px and 768px
+  },
+  DEKTOP: {
+    REGULAR: 'min-width: 769px', // between 769px and 1024px
+    LARGE: 'min-width: 1025px', // between 1025px and 1200px
+    X_LARGE: 'min-width: 1201px' // larger than 1201px
+  }
+}
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,3 +1,12 @@
 import colors from './colors.json'
 
 export const theme = { colors }
+
+// NEW
+export { Breakpoints } from './breakpoints'
+export { Color } from './colors'
+export { Spacing } from './spacing'
+export { Border } from './borders'
+export { Shadow } from './shadows'
+export { Transition } from './transitions'
+export { FontFamilly, FontSize, LineHeight, FontWeight } from './typography'
