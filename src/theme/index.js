77b1c6bf037760c01c03a1a1e1f5ac95962a0912
nyhvignoli <nyhv.contato@gmail.com>
2023-03-17 13:45:21 -0300

    theme(sizes): Add Sizes export

--- src/theme/index.js
+++ src/theme/index.js
@@ -6,6 +6,7 @@ export const theme = { colors }
 export { Breakpoints } from './breakpoints'
 export { Color } from './colors'
 export { Spacing } from './spacing'
+export { Sizes } from './sizes'
 export { Border } from './borders'
 export { Shadow } from './shadows'
 export { Transition } from './transitions'
