f728b1c18232f0c4a85eb1935f0a92c185a1cac8
nyhvignoli <nyhv.contato@gmail.com>
2023-03-27 14:38:55 -0300

    feat(Projects): Add new Gallery component to section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,22 +1,15 @@
 import React, { useContext, useState } from 'react'
 import { Wrapper } from './styles'
-import { Body4, Subhead } from '../../global/styles'
+import { Heading2 } from '../../global/styles'
 import ScreenShotsGallery from '../ScreenShotsGallery/ScreenShotsGallery'
 import { LanguageContext, Text } from '../../global/LanguagesContext'
 import { DATA_TYPE, getData } from '../../data'
-import { GalleryCard } from '../Cards/GalleryCard'
-import { ListWrapper } from '../Technologies/styles'
-import { Pill, PILL_SIZES } from '../Pill'
-import { Space } from '../Space'
-import { Color, Spacing } from '../../theme'
-import { IconButton } from '../Buttons'
-import { Chevron } from '../Icons'
-import { DIRECTIONS } from '../Icons/Chevron'
+import Gallery from '../Gallery/Gallery'
+import { GridLayout } from '../GridLayout'
 
 const Projects = () => {
   const { dictionary } = useContext(LanguageContext)
   const projects = getData(dictionary, DATA_TYPE.projects)
-
   const [imageDetailsOpen, setImageDetailsOpen] = useState(false)
   const [selectedProject, setSelectedProject] = useState(0)
 
@@ -31,58 +24,15 @@ const Projects = () => {
 
   return (
     <Wrapper imageDetailsOpen id="projects" data-testid="projects">
-      <Subhead data-aos="fade-zoom-in" data-aos-duration="1500">
+      <Heading2 data-aos="fade-zoom-in" data-aos-duration="1500">
         <Text tid="projects" />
-      </Subhead>
-      <IconButton>
-        <Chevron
-          color={Color.GREY_800}
-          dimensions={{ width: '36', height: '36' }}
+      </Heading2>
+      <GridLayout noPaddingOnMobile>
+        <Gallery
+          projects={projects}
+          handleImageDetailsOpen={handleImageDetailsOpen}
         />
-      </IconButton>
-      <IconButton disabled>
-        <Chevron
-          direction={DIRECTIONS.left}
-          color={Color.GREY_800}
-          dimensions={{ width: '36', height: '36' }}
-        />
-      </IconButton>
-      {projects.map((project, index) => {
-        return (
-          <GalleryCard
-            key={`${project.title}-${index}`}
-            headerProps={{
-              title: project.title,
-              subtitle: project.type,
-              cover: !!project.images?.length && {
-                src: project.images[0].src,
-                alt: project.images[0].alt
-              },
-              cta: project.type !== 'Back-end' && {
-                text: dictionary.gallery,
-                onClick: () => handleImageDetailsOpen(index)
-              }
-            }}
-            footerProps={{ ctas: project.ctas }}
-          >
-            <Space mobile={{ marginBottom: Spacing.MOBILE.X_SMALL }}>
-              <Body4>{project.description}</Body4>
-            </Space>
-            <ListWrapper>
-              {project.tags.map((item, index) => {
-                return (
-                  <Pill
-                    key={`${item}-${index}`}
-                    noBackground
-                    size={PILL_SIZES.small}
-                    text={item}
-                  />
-                )
-              })}
-            </ListWrapper>
-          </GalleryCard>
-        )
-      })}
+      </GridLayout>
       {imageDetailsOpen && (
         <ScreenShotsGallery
           handleImageDetailsClose={handleImageDetailsClose}
