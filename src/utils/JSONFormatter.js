ee4d962d3ae31d88eff3c500302760aab1e6b4a0
nyhvignoli <nyhv.contato@gmail.com>
2023-03-30 09:25:27 -0300

    feat(utils): Create JSONFormatter helper with toJSX function

---
+++ src/utils/JSONFormatter.js
@@ -0,0 +1,8 @@
+export const JSONFormatter = {
+  toJSX: (input = '') => {
+    return input.split('\n').map((item, i, array) => {
+      const isLast = i === array.length - 1
+      return isLast ? <>{item}</> : <>{item}<br/><br/></>
+    })
+  }
+}
