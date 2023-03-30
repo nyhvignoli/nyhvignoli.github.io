ad01d29453f629fb89426d5ca188434a07500544
nyhvignoli <nyhv.contato@gmail.com>
2023-03-30 09:33:00 -0300

    lint: Fix code format

--- src/utils/JSONFormatter.js
+++ src/utils/JSONFormatter.js
@@ -2,7 +2,15 @@ export const JSONFormatter = {
   toJSX: (input = '') => {
     return input.split('\n').map((item, i, array) => {
       const isLast = i === array.length - 1
-      return isLast ? <>{item}</> : <>{item}<br/><br/></>
+      return isLast ? (
+        <>{item}</>
+      ) : (
+        <>
+          {item}
+          <br />
+          <br />
+        </>
+      )
     })
   }
 }
