e585f68eb651d95c3eff56765725b78264a8bc8c
nyhvignoli <nyhv.contato@gmail.com>
2023-04-11 08:13:18 -0300

    refactor(Communities): Remove unused styles

--- src/components/Communities/styles.js
+++ src/components/Communities/styles.js
@@ -19,23 +19,3 @@ export const Wrapper = styled.section`
     padding: ${Spacing.DESKTOP.HUGE} 0px;
   }
 `
-
-export const Content = styled.div`
-  // padding: 0;
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
-  // gap: 5rem;
-  // width: 100%;
-
-  // @media (max-width: 1024px) {
-  //   display: grid;
-  //   grid-template-columns: 1fr 1fr;
-  //   text-align: center;
-  // }
-
-  // @media (max-width: 600px) {
-  //   display: flex;
-  //   flex-direction: column;
-  // }
-`
